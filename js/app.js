37c9e9362778dad12085e6cfc6cc1a6514139795
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-01-19 23:33:58 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+const container = document.querySelector(`.container`);
+const resultado = document.querySelector(`#resultado`);
+const formulario = document.querySelector(`#formulario`);
+
+window.addEventListener(`load`, ()=>{
+    formulario.addEventListener(`submit`, buscarClima);
+});
+
+function buscarClima(e){
+    e.preventDefault();
+
+    //Validar
+    const ciudad = document.querySelector(`#ciudad`).value;
+    const pais = document.querySelector(`#pais`).value;
+
+    if (ciudad ===`` || pais ===``){
+        //Hubo un error
+        mostrarError(`Ambos campos son obligatorios`);
+    }
+
+    //Consultar la API
+}
+
+function mostrarError(mensaje){
+    const alerta = document.querySelector(`.bg-red-100`);
+
+    if(!alerta) {
+        //Crear alerta
+        const alerta = document.createElement(`div`);
+
+        alerta.classList.add(`bg-red-100`,`border-red-400`,`text-red-400`,`text-red-700`,`px-4`,`py-3`, `rounded`,`max-w-md`,`mx-auto`,`mt-6`,`text-center`);
+
+        alerta.innerHTML=`
+            <strong class="font-bold">¡Error!</strong>
+            <span class="block">${mensaje}</span>
+        `;
+        container.appendChild(alerta);
+
+        //Eliminar alerta
+        setTimeout(()=>{
+            alerta.remove();
+        },3000)
+    }
+
+    
+}
